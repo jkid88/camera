ac33f934ca8f5637ddacb4c28a8c872dc5342091
jkid88 <83486132+jkid88@users.noreply.github.com>
2022-01-25 22:45:03 -0600

    Create camera.py

---
+++ camera.py
@@ -0,0 +1,14 @@
+import cv2
+import time
+import datetime
+
+cap = cv2.VideoCapture(0)
+
+while True:
+  _, frame = cap.read()
+
+  cv2.imshow("camera", frame)
+  if cv2.waitkey(1) == ord('q'):
+    break
+cap.release()
+cv2.destroyAllWindows()
